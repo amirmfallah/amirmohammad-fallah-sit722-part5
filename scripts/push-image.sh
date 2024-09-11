fd4d846174b4916b7ffd681cc9d128724ce60830
Amirmohammad Fallah <amirmfallah@yahoo.com>
2024-09-12 00:08:34 +1000

    debug pipeline

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -16,9 +16,6 @@
 set -u # or set -o nounset
 : "$CONTAINER_REGISTRY"
 : "$VERSION"
-: "$REGISTRY_UN"
-: "$REGISTRY_PW"
 
-echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin
 docker push $CONTAINER_REGISTRY/book-catalog:$VERSION
 docker push $CONTAINER_REGISTRY/inventory-management:$VERSION
